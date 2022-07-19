77324c34bcaf1a9566c7ba1a09e5df1ed816d8dd
Spencer Cain <cainspencerm@protonmail.com>
2022-07-19 09:08:58 -0400

    bug still not fixed that deploys image of size [4, 256, 256]

--- src/datasets/casia2.py
+++ src/datasets/casia2.py
@@ -75,9 +75,9 @@ class Casia2(Dataset):
             mask = Image.open(os.path.join(self._ground_truth_dir, gt_file))
             mask = self._mask_transform(mask)
 
-        if image.size()[0] == 4 or mask.size()[0] == 4:
-            image = image[:2]
-            mask = mask[:2]
+        if image.size()[0] != 3 or mask.size()[0] != 1:
+            image = image[:3]
+            mask = mask[0]
 
         return image, mask
 
