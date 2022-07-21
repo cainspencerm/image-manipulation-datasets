2eb0976a047e114b7e5a580d3729246657bdfcf0
Spencer Cain <cainspencerm@protonmail.com>
2022-07-20 22:02:15 -0400

    added transforms parameters and improved readability

--- src/datasets/casia2.py
+++ src/datasets/casia2.py
@@ -6,23 +6,24 @@ from PIL import Image
 
 
 class Casia2(Dataset):
-    def __init__(self, data_dir=None, split=None):
+    def __init__(self, data_dir: str='data', split: str='full', image_transform=None, mask_transform=None) -> None:
         super().__init__()
 
+        # Fetch the image filenames.
         self._authentic_dir = os.path.join(data_dir, 'Au')
-        self._tampered_dir = os.path.join(data_dir, 'Tp')
-
         auth_files = [f for f in os.listdir(self._authentic_dir) if '.tif' in f or '.jpg' in f]
-        tamp_files = [f for f in os.listdir(self._tampered_dir) if '.tif' in f or '.jpg' in f]
-
         auth_split_size = len(auth_files) // 10
+
+        self._tampered_dir = os.path.join(data_dir, 'Tp')
+        tamp_files = [f for f in os.listdir(self._tampered_dir) if '.tif' in f or '.jpg' in f]
         tamp_split_size = len(tamp_files) // 10
 
+        # Split the filenames into use cases.
         if split == 'train':
             self._input_files = auth_files[:auth_split_size * 8]
             self._input_files += tamp_files[:tamp_split_size * 8]
 
-        elif split == 'val':
+        elif split == 'valid':
             self._input_files = auth_files[auth_split_size * 8 : auth_split_size * 9]
             self._input_files += tamp_files[tamp_split_size * 8 : tamp_split_size * 9]
 
@@ -30,29 +31,40 @@ class Casia2(Dataset):
             self._input_files = auth_files[auth_split_size * 9:]
             self._input_files += tamp_files[tamp_split_size * 9:]
 
-        else:
+        elif split == 'full':
             self._input_files = auth_files + tamp_files
 
+        # Fetch the ground truth filenames.
         self._ground_truth_dir = os.path.join(data_dir, 'CASIA 2 Groundtruth')
-
         self._output_files = [f for f in os.listdir(self._ground_truth_dir) if f.endswith('.tif') or f.endswith('.jpg') or f.endswith('.png')]
 
-        self._image_transform = transforms.Compose([
-            transforms.Resize([256, 256]),
-            transforms.PILToTensor(),
-            transforms.ConvertImageDtype(torch.float),
-        ])
+        # Create transform callables for raw images and masks.
+        if image_transform is None:
+            self._image_transform = transforms.Compose([
+                transforms.Resize([256, 256]),
+                transforms.PILToTensor(),
+                transforms.ConvertImageDtype(torch.float),
+            ])
 
-        self._mask_transform = transforms.Compose([
-            transforms.Resize([256, 256]),
-            transforms.Grayscale(),
-            transforms.PILToTensor(),
-            transforms.ConvertImageDtype(torch.float),
-        ])
+        else:
+            self._image_transform = image_transform
+
+        if mask_transform is None:
+            self._mask_transform = transforms.Compose([
+                transforms.Resize([256, 256]),
+                # transforms.Grayscale(),
+                transforms.PILToTensor(),
+                transforms.ConvertImageDtype(torch.float),
+            ])
+        else:
+            self._mask_transform = mask_transform
 
     def __getitem__(self, idx):
+
         file = self._input_files[idx]
+
         if file.startswith('Au'):
+            # Load the image.
             image = Image.open(os.path.join(self._authentic_dir, file))
 
             if image.mode != 'RGB':
@@ -60,19 +72,31 @@ class Casia2(Dataset):
 
             image = self._image_transform(image)
 
+            # An authentic image has no manipulation mask.
             mask = torch.zeros(size=[1] + list(image.size()[1:]))
 
-        else:
+        elif file.startswith('Tp'):
+            # Load the image.
             image = Image.open(os.path.join(self._tampered_dir, file))
             image = self._image_transform(image)
 
+            # Find the corresponding ground truth file.
             tamp_id = file[-9:-4]
+            gt_file = None
             for f in self._output_files:
                 if tamp_id + '_gt' == f[-12:-4]:
                     gt_file = f
                     break
 
+            if gt_file is None:
+                raise ValueError('No ground truth file found for image: ' + file)
+
+            # Load the ground truth mask.
             mask = Image.open(os.path.join(self._ground_truth_dir, gt_file))
+
+            if mask.mode != 'L':
+                mask = mask.convert('L')
+
             mask = self._mask_transform(mask)
 
         return image, mask
